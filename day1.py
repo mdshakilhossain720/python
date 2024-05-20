5431217e3cd5fae98e6d425e3f501525ccea9661
shakil <mdshakilhossain720@gmail.com>
2024-05-20 17:51:40 +0600

    data varriable

--- day1.py
+++ day1.py
@@ -1,1 +1,30 @@
 print('Hello Python developer')
+name = "shakil"
+print(name)
+num =400
+print(num)
+dou =30.0
+print(dou)
+if True:
+    print('answer')
+else:
+    print('bad false')
+days=['stauarday','monday','tudeday']
+print(days)
+# multiline
+scentece="""Hellon balgladesh is the betys webiste in bd"""
+print(scentece)
+
+# opertor
+nam=str('shakil')
+a=int(500)
+y=float(50.0)
+print(nam)
+print(a)
+print(y)
+v=n=m=10
+print(n)
+
+for i in range (5):
+    print(i)
+
