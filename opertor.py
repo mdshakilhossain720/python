79156a581cdc580b2f79b08154fed27d6e2a03e9
shakil <mdshakilhossain720@gmail.com>
2024-05-21 16:06:12 +0600

    opertor

---
+++ opertor.py
@@ -0,0 +1,26 @@
+
+a=10
+b=20
+if(a == b):
+    print("Line one a equl to b")
+else:
+    print("line is the not quel")
+
+if(a !=b):
+    print("line is a is not quel to b")
+else:
+    print("line a qule to b")
+
+if(a >b):
+    print("a is big ")
+else:
+    print('b is the small')
+
+if (a<b):
+    print("a is small")
+else:
+    print("b is big")
+
+
+
+
