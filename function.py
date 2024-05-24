421c790f49b78d5f686bf035c87150e495f6b746
shakil <mdshakilhossain720@gmail.com>
2024-05-24 16:28:34 +0600

    functio call by value

--- function.py
+++ function.py
@@ -7,3 +7,38 @@ def printme( str ):
 # Now you can call printme function
 printme("I'm first call to user defined function!")
 printme("Again second call to the same function")
+
+
+def greetings(name):
+    "This is docstring of greetings function"
+    print("Hello {}".format(name))
+    return
+
+
+greetings("Samay")
+greetings("Pratima")
+greetings("Steven")
+
+
+def printinfo( name, age = 35 ):
+   "This prints a passed info into this function"
+   print ("Name: ", name)
+   print ("Age ", age)
+   return
+
+# Now you can call printinfo function
+printinfo( age=50, name="miki" )
+printinfo( name="miki" )
+
+def printinfo( name, age ):
+   "This prints a passed info into this function"
+   print ("Name: ", name)
+   print ("Age ", age)
+   return
+
+# Now you can call printinfo function
+# by positional arguments
+printinfo ("Naveen", 29)
+
+# by keyword arguments
+printinfo(name="miki", age = 30)
