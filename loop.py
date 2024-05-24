6eaeb76439ebff5c11d0405d3f33c846ffef075d
shakil <mdshakilhossain720@gmail.com>
2024-05-24 16:14:34 +0600

    function

--- loop.py
+++ loop.py
@@ -4,3 +4,11 @@ total = 0
 for num in numbers:
    total+=num
 print ("Total =", total)
+
+numbe = (34,54,67,21,78,97,45,44,80,19)
+for shakil in numbe:
+    if shakil %2==0:
+        print(shakil)
+
+for i in range(10,30):
+    print(i)
